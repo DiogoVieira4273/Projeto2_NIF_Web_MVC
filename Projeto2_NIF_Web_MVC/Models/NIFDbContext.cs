1a736b4a3d1e723eaac3a1be243e0aefd534e052
Diogo Major Vieira <diogomajor1993@outlook.pt>
2024-10-08 10:29:14 +0100

    Add project files.

---
+++ Projeto2_NIF_Web_MVC/Models/NIFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projeto2_NIF_Web_MVC.Models
+{
+    public class NIFDbContext : DbContext
+    {
+        public NIFDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<NIF_Empresa> NIF_Empresa { get; set; }
+    }
+}
