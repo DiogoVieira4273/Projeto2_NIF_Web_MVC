b1ca010157f1bb0a37b4e905b0d7273efa07f9d6
Diogo Major Vieira <diogomajor1993@outlook.pt>
2025-03-11 10:54:25 +0000

    Add project files.

---
+++ Projeto2_NIF_Web_MVC/Models/NIF_Empresa.cs
@@ -0,0 +1,20 @@
+﻿namespace Projeto2_NIF_Web_MVC.Models
+{
+    public class NIF_Empresa
+    {
+        public int ID { get; set; }
+        public int NIF { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PC4 { get; set; }
+        public int PC3 { get; set; }
+        public string Region { get; set; }
+        public string County { get; set; }
+        public string Parish { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Website { get; set; }
+        public int Fax { get; set; }
+        public string Imagem_URL { get; set; }
+    }
+}
